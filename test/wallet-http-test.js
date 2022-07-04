dc10acce829af9a7f63133c665c4c58be8771dd5
theanmolsharma <65589791+theanmolsharma@users.noreply.github.com>
2022-07-04 22:13:07 +0530

    test: modified tests to use old selection algorithm

--- test/wallet-http-test.js
+++ test/wallet-http-test.js
@@ -202,6 +202,7 @@ for (const witnessOpt of witnessOptions) {
     it('should send a tx', async () => {
       const options = {
         rate: 10000,
+        useSelectEstimate: true,
         outputs: [{
           value: 10000,
           address: addr.toString(node.network)
