lif-zone/lifcoin

mtx: added new selection algorithm `selectLowestLarger()`
Signed-off-by: theanmolsharma <anmolsharma0234@gmail.com>

lib/primitives/mtx.js
@@ -1912,6 +1912,96 @@ class CoinSelector {
   }
 
   /**
+   * Initiate selection using Lowest Larger selection algorithm.
+   * @param {Number} target - Selection target
+   * @returns {Number[]} selected - array of indicies of selected coins
+   */
+
+  selectLowestLarger(target) {
+    // while target is greater than
+    // the largest coin we have, we
+    // will keep selecting the largest coin
+
+    this.index = 0;
+    const selected = [];
+    let effectiveValue = this.utxos[this.index].effectiveValue;
+    while (target >= effectiveValue) {
+      // update target, select current coin and increment index
+      selected.push(this.index);
+      target -= effectiveValue;
+      this.index++;
+
+      if (this.index === this.utxos.length)
+        break;
+
+      effectiveValue = this.utxos[this.index].effectiveValue;
+    }
+
+    if (target > 0 && this.index !== this.utxos.length) {
+      // now we are sure that target < largest unselected coin
+      // we will perform Binary search to find the smallest coin
+      // which is greater than target value
+
+      const closestCoinIndex = this.findLowestLarger(target);
+      target -= this.utxos[closestCoinIndex].effectiveValue;
+      selected.push(closestCoinIndex);
+    }
+
+    return target > 0 ? [] : selected;
+  }
+
+  /**
+   * Find smallest coin greater than
+   * the target using binary search
+   * @param {Number} target
+   * @returns {Number} index
+   */
+
+  findLowestLarger(target) {
+    let i = this.index;
+    let j = this.utxos.length - 1;
+    // Corner cases
+    // if target < smallest coin available
+    // return the smallest coin
+    if (target <= this.utxos[j].effectiveValue)
+      return j;
+
+    // begin binary search
+    let mid = 0;
+    while (i < j) {
+      mid = Math.floor((i + j) / 2);
+
+      // calculate effective value of coin at mid
+      const effectiveValue = this.utxos[mid].effectiveValue;
+
+      if (effectiveValue === target)
+        return mid;
+
+      // if target is less than coin at mid
+      // then search in left part of array
+      if (target < effectiveValue) {
+        if (mid < this.utxos.length - 1) {
+          // calculate effective value of coin at mid + 1
+          const nextEffectiveValue = this.utxos[mid + 1].effectiveValue;
+          // if target is greater than next
+          // effective value, then return mid
+          // coin at mid > target > coin at mid + 1
+          if (target > nextEffectiveValue)
+            return mid;
+        }
+        // repeat for right half
+        i = mid + 1;
+      } else {
+        // repeat for left half
+        j = mid;
+      }
+    }
+
+    // only single element left after search
+    return mid;
+  }
+
+  /**
    * Calculate effectiveValue of a coin
    * effectiveValue = coin.value - cost of spending that coin
    * @param {Coin} coin - A coin object

theanmolsharma/bcoin

lib/primitives/mtx.js
@@ -1889,6 +1889,96 @@ class CoinSelector {
   }
 
   /**
+   * Initiate selection using Lowest Larger selection algorithm.
+   * @param {Number} target - Selection target
+   * @returns {Number[]} selected - array of indicies of selected coins
+   */
+
+  selectLowestLarger(target) {
+    // while target is greater than
+    // the largest coin we have, we
+    // will keep selecting the largest coin
+
+    this.index = 0;
+    const selected = [];
+    let effectiveValue = this.coins[this.index].effectiveValue;
+    while (target >= effectiveValue) {
+      // update target, select current coin and increment index
+      selected.push(this.index);
+      target -= effectiveValue;
+      this.index++;
+
+      if (this.index === this.coins.length)
+        break;
+
+      effectiveValue = this.coins[this.index].effectiveValue;
+    }
+
+    if (target > 0 && this.index !== this.coins.length) {
+      // now we are sure that target < largest unselected coin
+      // we will perform Binary search to find the smallest coin
+      // which is greater than target value
+
+      const closestCoinIndex = this.findLowestLarger(target);
+      target -= this.coins[closestCoinIndex].effectiveValue;
+      selected.push(closestCoinIndex);
+    }
+
+    return target > 0 ? [] : selected;
+  }
+
+  /**
+   * Find smallest coin greater than
+   * the target using binary search
+   * @param {Number} target
+   * @returns {Number} index
+   */
+
+  findLowestLarger(target) {
+    let i = this.index;
+    let j = this.coins.length - 1;
+    // Corner cases
+    // if target < smallest coin available
+    // return the smallest coin
+    if (target <= this.coins[j].effectiveValue)
+      return j;
+
+    // begin binary search
+    let mid = 0;
+    while (i < j) {
+      mid = Math.floor((i + j) / 2);
+
+      // calculate effective value of coin at mid
+      const effectiveValue = this.coins[mid].effectiveValue;
+
+      if (effectiveValue === target)
+        return mid;
+
+      // if target is less than coin at mid
+      // then search in left part of array
+      if (target < effectiveValue) {
+        if (mid < this.coins.length - 1) {
+          // calculate effective value of coin at mid + 1
+          const nextEffectiveValue = this.coins[mid + 1].effectiveValue;
+          // if target is greater than next
+          // effective value, then return mid
+          // coin at mid > target > coin at mid + 1
+          if (target > nextEffectiveValue)
+            return mid;
+        }
+        // repeat for right half
+        i = mid + 1;
+      } else {
+        // repeat for left half
+        j = mid;
+      }
+    }
+
+    // only single element left after search
+    return mid;
+  }
+
+  /**
    * Calculate effectiveValue of a coin
    * effectiveValue = coin.value - cost of spending that coin
    * @param {Coin} coin - A coin object