lif-zone/lifcoin

mtx: added new coin selection algorithm `selectBnB()`
Signed-off-by: theanmolsharma <anmolsharma0234@gmail.com>

lib/primitives/mtx.js
@@ -1912,6 +1912,92 @@ class CoinSelector {
   }
 
   /**
+   * Initiate selection using Branch and Bound selection.
+   * @param {Number} target - Selection target
+   * @param {Number} costOfChange - Cost of producing and spending change
+   * @returns {Number[]} selected - array of indicies of selected coins
+   */
+
+  async selectBnB(target, costOfChange) {
+    const selected = [];
+    let currValue = 0;
+    let totalValue = 0;
+    let index = 0;
+    for (const utxo of this.utxos)
+      totalValue += utxo.effectiveValue;
+
+    // we don't have enough funds
+    if (totalValue < target) {
+      throw new FundingError(
+        'Not enough funds.',
+        totalValue,
+        target);
+    }
+
+    let tries = 100000;
+    let bestWaste = CoinSelector.MAX_MONEY;
+    let bestSelection = [];
+    // perform depth-first search for choosing UTXOs
+    while (tries-- > 0) {
+      let backtrack = false;
+      // conditions for backtracking
+      // 1. cannot reach target with remaining amount
+      // 2. selected value is greater than upper bound
+      if (currValue + totalValue < target
+        || currValue > target + costOfChange) {
+        backtrack = true;
+      } else if (currValue >= target) {
+        // we have a solution, we will compare it with current best solution
+        // we will try to find the solution with minimum waste
+        const currWaste = this.getWaste(selected, 0, target);
+        if (currWaste <= bestWaste) {
+          bestSelection = selected.slice();
+          bestWaste = currWaste;
+          if (bestWaste <= 0)
+            break;
+        }
+        backtrack = true;
+      }
+
+      // backtracking here
+      if (backtrack) {
+        // we have backtracked to the first UTXO,
+        // all branches are traversed, we are done here
+        if (selected.length === 0)
+          break;
+
+        // Add omitted UTXOs back before traversing
+        // the omission branch of last included UTXO
+        // we are using a while loop here beacuse
+        // there can be more than one UTXOs that were omitted
+        while (--index > selected[selected.length - 1]) {
+          totalValue += this.utxos[index].effectiveValue;
+        }
+
+        // Remove last included UTXO from selected list.
+        currValue -= this.utxos[index].effectiveValue;
+        selected.pop();
+      } else {
+        const utxo = this.utxos[index];
+        // remove this UTXO from total available amount
+        totalValue -= utxo.effectiveValue;
+        // if this UTXO is the first one or
+        // if the previous index is included or
+        // if this UTXO's value is different from the previous one
+        if  (selected.length === 0
+          || index - 1 === selected[selected.length - 1]
+          || utxo.effectiveValue !== this.utxos[index - 1].effectiveValue) {
+          selected.push(index);
+          currValue += this.utxos[index].effectiveValue;
+        }
+      }
+      index++;
+    }
+
+    return bestSelection;
+  }
+
+  /**
    * Initiate selection using Lowest Larger selection algorithm.
    * @param {Number} target - Selection target
    * @returns {Number[]} selected - array of indicies of selected coins

theanmolsharma/bcoin

lib/primitives/mtx.js
@@ -1889,6 +1889,92 @@ class CoinSelector {
   }
 
   /**
+   * Initiate selection using Branch and Bound selection.
+   * @param {Number} target - Selection target
+   * @param {Number} costOfChange - Cost of producing and spending change
+   * @returns {Number[]} selected - array of indicies of selected coins
+   */
+
+  async selectBnB(target, costOfChange) {
+    const selected = [];
+    let currValue = 0;
+    let totalValue = 0;
+    let index = 0;
+    for (const coin of this.coins)
+      totalValue += coin.effectiveValue;
+
+    // we don't have enough funds
+    if (totalValue < target) {
+      throw new FundingError(
+        'Not enough funds.',
+        totalValue,
+        target);
+    }
+
+    let tries = 100000;
+    let bestWaste = CoinSelector.MAX_MONEY;
+    let bestSelection = [];
+    // perform depth-first search for choosing UTXOs
+    while (tries-- > 0) {
+      let backtrack = false;
+      // conditions for backtracking
+      // 1. cannot reach target with remaining amount
+      // 2. selected value is greater than upper bound
+      if (currValue + totalValue < target
+        || currValue > target + costOfChange) {
+        backtrack = true;
+      } else if (currValue >= target) {
+        // we have a solution, we will compare it with current best solution
+        // we will try to find the solution with minimum waste
+        const currWaste = await this.getWaste(selected, 0, target);
+        if (currWaste <= bestWaste) {
+          bestSelection = selected.slice();
+          bestWaste = currWaste;
+          if (bestWaste <= 0)
+            break;
+        }
+        backtrack = true;
+      }
+
+      // backtracking here
+      if (backtrack) {
+        // we have backtracked to the first UTXO,
+        // all branches are traversed, we are done here
+        if (selected.length === 0)
+          break;
+
+        // Add omitted UTXOs back before traversing
+        // the omission branch of last included UTXO
+        // we are using a while loop here beacuse
+        // there can be more than one UTXOs that were omitted
+        while (--index > selected[selected.length - 1]) {
+          totalValue += this.coins[index].effectiveValue;
+        }
+
+        // Remove last included UTXO from selected list.
+        currValue -= this.coins[index].effectiveValue;
+        selected.pop();
+      } else {
+        const coin = this.coins[index];
+        // remove this UTXO from total available amount
+        totalValue -= coin.effectiveValue;
+        // if this UTXO is the first one or
+        // if the previous index is included or
+        // if this UTXO's value is different from the previous one
+        if  (selected.length === 0
+          || index - 1 === selected[selected.length - 1]
+          || coin.effectiveValue !== this.coins[index - 1].effectiveValue) {
+          selected.push(index);
+          currValue += this.coins[index].effectiveValue;
+        }
+      }
+      index++;
+    }
+
+    return bestSelection;
+  }
+
+  /**
    * Initiate selection using Lowest Larger selection algorithm.
    * @param {Number} target - Selection target
    * @returns {Number[]} selected - array of indicies of selected coins