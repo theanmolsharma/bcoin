lif-zone/lifcoin

mtx: added waste metric function `getWaste()` to calculate waste for a selection
Signed-off-by: theanmolsharma <anmolsharma0234@gmail.com>

lib/primitives/mtx.js
@@ -2013,6 +2013,40 @@ class CoinSelector {
     const effectiveValue = coin.value - this.getFee(spendingSize);
     return effectiveValue;
   }
+
+  /**
+   * Calculate waste for a selection
+   * @param {Number[]} selected - indicies of selected coins
+   * @param {Number} costOfChange - the cost of making change and spending it
+   * @param {Number} target - selection target
+   * @returns {Number} - waste
+   */
+
+  getWaste(selected, costOfChange, target) {
+    let waste = 0;
+    let selectedAmount = 0;
+    // calculate current fee rate in sats/byte
+    const currentFeeRate = this.getFee(1);
+
+    for (const i of selected) {
+      const utxo = this.utxos[i];
+      selectedAmount += utxo.effectiveValue;
+      // Consider the cost of spending an input now vs in the future
+      waste += (currentFeeRate - CoinSelector.LONG_TERM_FEERATE)
+        * utxo.spendingSize;
+    }
+
+    if (costOfChange > 0) {
+      // Consider the cost of making change and spending it in the future
+      waste += costOfChange;
+    } else {
+      // if costOfChange is not set,
+      // consider the excess we are throwing away to fees
+      waste += selectedAmount - target;
+    }
+
+    return waste;
+  }
 }
 
 /**
@@ -2041,6 +2075,22 @@ CoinSelector.MIN_FEE = 10000;
  */
 
 CoinSelector.MAX_FEE = consensus.COIN / 10;
+
+/**
+ * Maximum possible supply of Bitcoin
+ * @const {Amount}
+ * @default
+ */
+
+CoinSelector.MAX_MONEY = 21000000 * 100000000;
+
+/**
+ * Long term feerate (sats/vbyte)
+ * @const {Amount}
+ * @default
+ */
+
+CoinSelector.LONG_TERM_FEERATE = 5;
 
 /**
  * Funding Error

theanmolsharma/bcoin

lib/primitives/mtx.js
@@ -1990,6 +1990,40 @@ class CoinSelector {
     const effectiveValue = coin.value - this.getFee(spendingSize);
     return effectiveValue;
   }
+
+  /**
+   * Calculate waste for a selection
+   * @param {Number[]} selected - indicies of selected coins
+   * @param {Number} costOfChange - the cost of making change and spending it
+   * @param {Number} target - selection target
+   * @returns {Number} - waste
+   */
+
+  async getWaste(selected, costOfChange, target) {
+    let waste = 0;
+    let selectedAmount = 0;
+    // calculate current fee rate in sats/byte
+    const currentFeeRate = this.getFee(1);
+
+    for (const i of selected) {
+      const coin = this.coins[i];
+      selectedAmount += coin.effectiveValue;
+      // Consider the cost of spending an input now vs in the future
+      const spendingSize = await coin.estimateSpendingSize(this.getAccount);
+      waste += (currentFeeRate - CoinSelector.LONG_TERM_FEERATE) * spendingSize;
+    }
+
+    if (costOfChange > 0) {
+      // Consider the cost of making change and spending it in the future
+      waste += costOfChange;
+    } else {
+      // if costOfChange is not set,
+      // consider the excess we are throwing away to fees
+      waste += selectedAmount - target;
+    }
+
+    return waste;
+  }
 }
 
 /**
@@ -2018,6 +2052,22 @@ CoinSelector.MIN_FEE = 10000;
  */
 
 CoinSelector.MAX_FEE = consensus.COIN / 10;
+
+/**
+ * Maximum possible supply of Bitcoin
+ * @const {Amount}
+ * @default
+ */
+
+CoinSelector.MAX_MONEY = 21000000 * 100000000;
+
+/**
+ * Long term feerate (sats/vbyte)
+ * @const {Amount}
+ * @default
+ */
+
+CoinSelector.LONG_TERM_FEERATE = 5;
 
 /**
  * Funding Error